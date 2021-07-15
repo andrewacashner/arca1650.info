397782cf714d6e071176724036e03db37b7a2b17
Andrew A. Cashner <andrewacashner@gmail.com>
2021-07-14 21:20:10 -0400

    add simple css

--- www/compose.php
+++ www/compose.php
@@ -103,6 +103,8 @@ $mei = addslashes($mei);
     <head>
         <title>Arca musarithmica output</title>
         <meta charset="utf-8"/> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" type="text/css" href="arca.css"/>
     </head>
     <body>
     <header>
