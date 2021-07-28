d232d80b1d5ebc6d15153e29ea8a399b8945783f
Andrew A. Cashner <andrewacashner@gmail.com>
2021-07-28 10:25:31 -0400

    Improve upload script with rsync to only upload updated files and delete extraneous ones; relative DTD links in input text directories; remove 'mixed' texts

--- upload.sh
+++ upload.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env sh
+#
 # Upload www directory to public_html directory on server
-scp -P 7822 -i ~/.ssh/id_rsa-a2 -r www/* arcainfo@arca1650.info:~/public_html
+#
+# Only upload changed, recently modified files
+# Delete extraneous files
+rsync -avuzh --delete \
+    -e "ssh -p 7822 -i $HOME/.ssh/id_rsa-a2" \
+    ./www/ arcainfo@arca1650.info:~/public_html/
+
+
+# For reference, this is how to do it with scp, but it lacks a lot of
+# important features (deleting extraneous files, only updating changed files)
+# scp -P 7822 -i ~/.ssh/id_rsa-a2 -r www/* arcainfo@arca1650.info:~/public_html
