a6c546ca8ac4bc58bc7707b9d4ad27ac0554b3c7
Andrew A. Cashner <andrewacashner@gmail.com>
2021-08-02 22:03:24 -0400

    local testing of midi

--- www/compose.php
+++ www/compose.php
@@ -115,6 +115,11 @@ $mei = addslashes($rawMei);
                    Kircher’s device for automatic music composition from his
                    Musurgia universalis (Rome, 1650), Book VIII, in Haskell 
                    implementation by Andrew Cashner (Rochester, New York, 2021)" />
+
+<script type="text/javascript" src="https://www.verovio.org/javascript/develop/verovio-toolkit.js"></script>
+
+<script type="text/javascript" src="https://www.midijs.net/lib/midi.js"></script>
+
 <script>
 console.log("Setting input text [<?=$inputText?>] in method [<?=$inputType?>]: style [<?=$inputStyle?>], mode [<?=$inputMode?>], meter [<?=$inputMeter?>]...");
 
@@ -122,19 +127,18 @@ console.log("Loading input file [<?=$infileName?>]...");
 
 </script>
 
-<!--
-<script type="text/javascript" src="https://www.verovio.org/javascript/develop/verovio-toolkit.js"></script>
-
-<script type="text/javascript" src="https://www.midijs.net/lib/midi.js"></script>
-
 <script type="text/javascript">
 // Use Verovio toolkit to get MIDI data as string, add header
 const vrvToolkit = new verovio.toolkit();
 const base64midi = vrvToolkit.renderToMIDI('<?=$mei?>');
 const song = 'data:audio/midi;base64,' + base64midi;
 console.log("Ready to play MIDI data beginning %s...", song.substring(0,100));
+
+function doMidi() {
+    MIDIjs.play(song);
+}
+
 </script>
--->
 
 <script type="module">
 // Put the Arca output from PHP into a variable and load it into Verovio
@@ -171,11 +175,9 @@ app.loadData(mei);
 
             <p><a href="compose.html">Compose more music</a></p>
 
-<!--
             <div class="playback-controls">
-                <button onclick="MIDIjs.play(song);">Listen (MIDI)</button>
+                <button onclick="doMidi();">Listen (MIDI)</button>
             </div>
--->
             <div class="panel-body">
                 <div id="app" class="panel">
 <noscript>
