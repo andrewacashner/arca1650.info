0f305911c9730234edaee1c0dd6609be446a446b
Andrew A. Cashner <andrewacashner@gmail.com>
2021-08-06 08:54:51 -0400

    More improved IO chart format, acknowledgments, start of composition discussion

--- www/img/iochart.js
+++ www/img/iochart.js
@@ -7,47 +7,47 @@ function drawChart() {
     data.addColumn('string', 'to');
     data.addColumn('number', 'type'); 
     data.addRows([
-
         [ 'style',          'syntagma',         2 ],
         [ 'text meter',     'pinax',            2 ],
         [ 'syllable text',  'column',           2 ],
         [ 'syllable length', 'column',          2 ],
         [ 'line position',  'column',           1 ],
-        
+
         [ 'music meter',    'meter symbols',    2 ],
-        [ 'music meter',    'rperm meters',     2 ], 
         [ 'meter symbols',  'meter signature',  1 ],
-        
-        [ 'mode',           'mode systems',     2 ],
-        [ 'mode systems',   'key signature',    1 ],
+        [ 'music meter',    'rperm meters',     2 ], 
+
         [ 'mode',           'mode scales',      2 ],
+        [ 'mode scales',    'key signature',    1 ],
         
         [ 'syntagma',       'pinax',            1 ],
         [ 'pinax',          'column',           1 ],
         [ 'column',         'vperm',            1 ],
         [ 'column',         'rperm meters',     1 ],
         [ 'rperm meters',   'rperm',            1 ],
-
-        [ 'vperm index (random)', 'vperm',      1 ],
-        [ 'rperm index (random)', 'rperm',      1 ],
-        [ 'vperm',          'pitch class',      1 ],
         [ 'rperm',          'duration',         1 ],
+        [ 'choice/random',  'rperm',            1 ],
+
+        [ 'choice/random',  'vperm',            1 ],
+        [ 'vperm',          'pitch class',      1 ],
 
         [ 'pitch class',    'mode scales',      1 ],
         [ 'mode scales',    'accidental',       1 ],
 
-        [ 'pitch class',    'pitch name',       1 ],
+        [ 'mode scales',    'pitch name',       1 ],
         [ 'pitch class',    'clefs/ranges',     1 ],
         [ 'clefs/ranges',   'clef',             1 ],
         [ 'clefs/ranges',   'octave',           1 ]
     ]);
 
     var options = {
-        width: 600,
-        height: 400,
+        width: 577,
+        height: 367,
+        tooltip: { isHtml: true },
         sankey: {
             node: { label: { bold: true }
-                    , nodePadding : 25
+                    , nodePadding: 37
+                    , interactivity: true
                   }
             , link: {
                 colorMode: 'gradient'
