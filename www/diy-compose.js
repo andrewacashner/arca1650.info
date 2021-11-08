fa0f1d471f931ae6e6639008366e583565514f76
Andrew A. Cashner <andrewacashner@gmail.com>
2021-11-08 16:19:05 -0500

    Update arca-exe after mistake that disabled it; add test diy-text page

---
+++ www/diy-compose.js
@@ -0,0 +1,131 @@
+/* Write your own text for the Arca musarithmica
+ *
+ * 2021/11/08
+ */
+
+/* Make a 'select' element with options appropriate for the given style.
+ * Remove any options that may have been set before.
+ *
+ * Arguments: 
+ *  (1) element         -- HTML element node
+ *  (2) optionsPerStyle -- Object with two nested sets of properties: one for
+ *                         "simple" style, the other for "florid"; within
+ *                         each the property keys are the option's 'value'
+ *                         attribute of and the key is the text
+ *  (3) style           -- String, "simple" or "florid"
+ *
+ *  Returns the document element with updated options.
+ */
+function createOptions(element, optionsPerStyle, style) {
+    
+    // Clear all options in case any have already been set
+    element = removeChildren(element);
+
+    // Write the options relevant for this style with their value labels
+    let options = optionsPerStyle[style];
+    for (let o in options) {
+        var thisOpt = new Option(options[o], o);
+        element.add(thisOpt);
+    }
+
+    return element;
+}
+
+// Return a node with no children; clear all options in case any have already been set.
+function removeChildren(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+    return node;
+}
+
+/* DIY MODE
+ *
+/* Conditional dropdown menus in do-it-yourself tone, where user selects
+ * style, text, tone, and meter
+ */
+
+// Update musicMeter select with appropriate meter options for each style
+function diyMeterOptions(style) {
+    console.log("Displaying meter options for style '%s'", style);
+    
+    let meters = {
+        "simple": {
+            unselected:    "Select a musical meter (mensuration):",
+            "Duple":       "Duple meter (alla breve, cut C)",
+            "TripleMinor": "Triple meter, three minims per unit (C3)",
+            "TripleMajor": "Triple meter, three semibreves per unit (cut C3)"
+        },
+        "florid": {
+            "Duple":  "Duple meter (C), only option in florid style",
+        }
+    };
+
+    var musicMeter = document.getElementById("musicMeter");
+    musicMeter = createOptions(musicMeter, meters, style);
+
+    return true;
+}
+
+/* Update 'tone' select element with the correct options for permissible tones
+ * for given style ("simple" or "florid") and text ("inputText" value).
+ *
+ * 'Text' argument is optional; if it is omitted we just reset the default
+ * option.
+ */
+function diyToneOptions(style, text) { 
+    console.log("Displaying tone options for style '%s', text '%s'", style, text);
+
+    let legalTones = {
+        "simple": {
+            "DIY":                  // DIY
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        },
+        "florid": {
+            "DIY":                  // DIY
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        }
+    };
+
+    let toneLabels = {
+        "Tone0":  "Select a tone or mood",
+        "Tone1":  "Tone I: Pious, religious",
+        "Tone2":  "Tone II: Cheerful, easygoing",
+        "Tone3":  "Tone III: Tearful",
+        "Tone4":  "Tone IV: Sad, mournful",
+        "Tone5":  "Tone V: Heroic, lively",
+        "Tone6":  "Tone VI: Warlike",
+        "Tone7":  "Tone VII: Delightful",
+        "Tone8":  "Tone VIII: Joyful",
+        "Tone9":  "Tone IX: Confident",
+        "Tone10": "Tone X: Calm, mild",
+        "Tone11": "Tone XI: Majestic",
+        "Tone12": "Tone XII: Grave, austere"
+    };
+
+
+    // Clear all options in case any have already been set
+    let toneSelect = document.getElementById("tone");
+    toneSelect = removeChildren(toneSelect);
+
+    // Reset the first option
+    var firstOpt = new Option(toneLabels["Tone0"], "");
+    toneSelect.add(firstOpt);
+
+    // If the text was omitted, just stop here
+    if (text != undefined) {
+        // Only add as many tones as allowed
+        // Pull out the labels for tones included in the table
+        let tones = legalTones[style][text];
+        for (let m of tones) {
+            let toneName = "Tone" + m;
+            var opt = new Option(toneLabels[toneName], toneName);
+            toneSelect.add(opt);
+        }
+    }
+
+    return true;
+}
+
+
+
--- www/diy-compose.php
+++ www/diy-compose.php
@@ -1,107 +1,51 @@
 <?php
 # Web interface to arca using MEI output displayed via Verovio
-# 2021/07/14
+# For DIY texts created on the site
+# 2021/11/08
 #
 # Run the arca to generate MEI music output, given a choice of input texts
 # from a webform. Display the MEI using the Verovio web app.
 #
-# Take the choice of input text from the form in index.html,
-# select the correct XML input file from the array of file names,
-# construct the input and output filenames, and then pass these as arguments
-# to 'arca-exe'.
-# 
-# For DIY files, where users set the input parameters instead of them being
-# preset in the XML input, we have to subsitute the user's input choices
-# instead of the placeholders in the XML input file.
+# Input the user's choices into an XML shell file, construct the input and
+# output filenames, and then pass these as arguments to 'arca-exe'.
 # 
 # We store the standard output from 'arca-exe' in a PHP variable, which we
 # then pass to Javascript and load into the Verovio app.
 
 # GET INPUT
 # Input values from HTML form
-$inputText  = $_POST['inputText'];
-$inputType  = $_POST['inputType'];
-$inputStyle = $_POST['style'];
-$inputTone  = $_POST['tone'];
-$inputMeter = $_POST['musicMeter'];
+$title       = $_POST['inputTextTitle'];
+$wordsAuthor = $_POST['inputTextWordsAuthor'];
+$inputText   = $_POST['inputText'];
+$inputTone   = $_POST['tone'];
+$inputMeter  = $_POST['musicMeter'];
 
-# File basenames for input and output
-$baseName = array(
-    "Ave_maris_stella" => "Ave_maris_stella"
-    , "Ave_Regina"       => "Ave_Regina_Angelorum"
-    , "Bernardus"        => "Bernardus-Hora_novissima"
-    , "Boethius"         => "Boethius-Nubibus_atris"
-    , "Catullus"         => "Catullus-Viuamus_mea_Lesbia"
-    , "Horace"           => "Horace-Maecenas_atavis_edite_regibus"
-    , "Iste_confessor"   => "Iste_confessor_Domini"
-    , "Ps150"            => "Ps-150"
-    , "Shakespeare"      => "Shakespeare-If_Music_Be"
-    , "Stephanus"        => "Stephanus-O_ter_quaterque_felix_Cicada"
-    , "Veni_creator"     => "Veni_creator_Spiritus"
-);
-
-# File titles to be used in the generated HTML output
-$fileTitle = array(
-    "Ave_maris_stella" => "Ave maris stella (Iambic Euripidaeic meter)"
-    , "Ave_Regina"       => "Ave Regina Angelorum (Iambic Enneasyllabic meter)"
-    , "Bernardus"        => "Bernardus Melanensis, Hora novissima (Dactylic/Adonic meter)"
-    , "Boethius"         => "Boethius, Nubibus atriis (Adonic meter)"
-    , "Catullus"         => "Catullus, Viuamus mea Lesbia atque amemus (Hendecasyllabic meter)"
-    , "Horace"           => "Horace, Maecenas atavis edite regibus (Dodecasyllabic meter)"
-    , "Iste_confessor"   => "Iste confessor Domini (Sapphic meter)"
-    , "Ps150"            => "Psalmi CL (Irregular meter/Prose)"
-    , "Shakespeare"      => "Shakespeare, If Music Be the Food of Love (Decasyllabic meter = Iambic pentameter)"
-    , "Stephanus"        => "Stephanus, O ter quaterque felix Cicada (Anacreontic meter)"
-    , "Veni_creator"     => "Veni creator Spiritus (Iambic Archilochic meter)"
-);
-
-$title = "$fileTitle[$inputText]";
-
-# Style selection to be inserted into XML input (maps to Haskell Style data type)
-$style = array(
-    "simple" => "Simple"
-    , "florid" => "Florid"
-);
-
+# Clean up input text
+$inputText = str_replace("\r\n", "</l><l>", $inputText);
 
 # SET UP INPUT AND OUTPUT FILES and RUN THE ARK
-$fileBasename = "$baseName[$inputText]";
-
 $arca = "./cgi-bin/arca-exe";
 
 # Set UTF-8 encoding to preserve Unicode in XML output of arca in standard
 # output of server's shell
 putenv('LANG=en_US.UTF-8');
 
-if ($inputType == "diy") {
+# For DIY files, users set their own parameters for style, meter, and
+# tone.  The input files have placeholder strings for these XML
+# attributes, so we read in the input file, and replace the placeholders
+# with the values taken from the HTML form input.  We pass this modified
+# file text (as string) to arca in the shell.
+$infileName  = "input/text/diy.xml";
 
-    # For DIY files, users set their own parameters for style, meter, and
-    # tone.  The input files have placeholder strings for these XML
-    # attributes, so we read in the input file, and replace the placeholders
-    # with the values taken from the HTML form input.  We pass this modified
-    # file text (as string) to arca in the shell.
+$fileString  = file_get_contents($infileName);
+$fileString  = str_replace(
+    array("{title}", "{wordsAuthor}", "{musicMeter}", "{tone}", "{inputText}"),
+    array($title, $wordsAuthor, $inputMeter, $inputTone, $inputText), 
+    $fileString);
 
-    $infileName  = "input/text/$fileBasename.xml";
+$fileString = escapeshellarg($fileString);
 
-    $fileString  = file_get_contents($infileName);
-    $fileString  = str_replace(
-        array("{style}", "{musicMeter}", "{tone}"),
-        array($style[$inputStyle], $inputMeter, $inputTone), 
-        $fileString);
-
-    $fileString = escapeshellarg($fileString);
-
-    $rawMei = shell_exec("echo {$fileString} | {$arca} - -");
-
-} else {
-
-    # For prepared files, we just select the correct input file based on the
-    # style and run arca on that.
-
-    $infileName  = "input/prepared/$inputStyle/$fileBasename.xml";
-
-    $rawMei = shell_exec("{$arca} {$infileName} -");
-}
+$rawMei = shell_exec("echo {$fileString} | {$arca} - -");
 
 # Escape XML slashes for safe transfer to Javascript
 $mei = addslashes($rawMei);
@@ -130,7 +74,7 @@ $mei = addslashes($rawMei);
         }
         window.meiXML = mei;
 
-        console.log("Setting input text [<?=$inputText?>] in method [<?=$inputType?>]: style [<?=$inputStyle?>], tone [<?=$inputTone?>], meter [<?=$inputMeter?>]...");
+        console.log("Setting input text in diy method, tone [<?=$inputTone?>], meter [<?=$inputMeter?>]...");
         console.log("Loading input file [<?=$infileName?>]...");
         console.log("Rendering MEI output file beginning [%s]...", window.meiXML.substring(0,300));
     </script>
@@ -197,7 +141,7 @@ $mei = addslashes($rawMei);
     <body>
     <header>
       <h1>ARCA MUSARITHMICA</h1>
-      <h2>a device for automatic music composition from 1650</h1>
+      <h2>a device for automatic music composition from 1650</h2>
     </header>
       <nav>
         <ul>
